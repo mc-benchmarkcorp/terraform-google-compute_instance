7199c5b5493342ac7478612d8b5811276713244b
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 09:12:50 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "network" {
+  description = "Network to deploy to. Only one of network or subnetwork should be specified."
+  default     = ""
+}
+
+variable "subnetwork" {
+  description = "Subnet to deploy to. Only one of network or subnetwork should be specified."
+  default     = ""
+}
+
+variable "subnetwork_project" {
+  description = "The project that subnetwork belongs to"
+  default     = ""
+}
+
+variable "hostname" {
+  description = "Hostname of instances"
+  default     = ""
+}
+
+variable "static_ips" {
+  type        = list(string)
+  description = "List of static IPs for VM instances"
+  default     = []
+}
+
+variable "access_config" {
+  description = "Access configurations, i.e. IPs via which the VM instance can be accessed via the Internet."
+  type = list(object({
+    nat_ip       = string
+    network_tier = string
+  }))
+  default = []
+}
+
+variable "num_instances" {
+  description = "Number of instances to create. This value is ignored if static_ips is provided."
+  default     = "1"
+}
+
+variable "instance_template" {
+  description = "Instance template self_link used to create compute instances"
+}
+
+variable "region" {
+  type        = string
+  description = "Region where the instances should be created."
+  default     = null
+}
