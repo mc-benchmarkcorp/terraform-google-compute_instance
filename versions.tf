44bd13016d0bb688accde74b776cc7964f1b69d8
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 09:18:29 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,4 +3,7 @@ terraform {
   required_providers {
     google = ">= 3.43, <4.0"
   }
+  provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-vm:compute_instance/v6.2.0"
   }
+}
