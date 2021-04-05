59668fd54fa709a3aba42dfe6e439ffd0eabe7f9
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 09:54:01 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,3 @@ output "instance_zone" {
     value = google_compute_instance.default.*.zone
     description = "The zone where the instance will be created"
 }
-output "subnet_name" {
-    value = google_compute_instance.default[*].network_interface.subnetwork
-    description = "The names of the subnetwork the instances will be located"
-}
-output "instance_ip_addr" {
-  value = google_compute_instance.default[*].network_interface.[*].access_config.[*].nat_ip
-} 
