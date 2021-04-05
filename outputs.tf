69edb278ceea6250cf1ac8dd49093a28ad5a6f6c
Matt Cole <55399416+mc-benchmarkcorp@users.noreply.github.com>
2021-04-05 13:03:11 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
   
 output "name" {
-    value = google_compute_instance.default.*.name
+    value = google_compute_instance.default.name
     description = "The names of the instance being created"
 }
 output "instance_zone" {
-    value = google_compute_instance.default.*.zone
+    value = google_compute_instance.default.zone
     description = "The zone where the instance will be created"
 }
